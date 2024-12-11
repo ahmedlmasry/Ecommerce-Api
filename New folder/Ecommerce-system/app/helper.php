270c47c9c990483fcbe04ad6c3e16f64676a213b
ahmedlmasry <elmasryahmed162@gmail.com>
2024-12-11 13:54:08 +0200

    initial commit

--- New folder/Ecommerce-system/app/helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function responseJson($status,$message,$data=null){
-        $response = [
-            'status'=> $status,
-            'message'=> $message,
-            'data' =>$data,
-        ];
-        return response($response);
-    }
